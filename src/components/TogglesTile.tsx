91d38065e3b30103aef96a134950c49b0b5a1fd5
unknown <jacobdcutts@gmail.com>
2022-06-29 17:00:28 +0100

    removed unused logo import

--- src/components/TogglesTile.tsx
+++ src/components/TogglesTile.tsx
@@ -1,14 +1,14 @@
 import ToggleQuestion from "./ToggleQuestion";
-import officeConditions from "../data/officeConditions.json";
+// import officeConditions from "../data/officeConditions.json";
 
 export default function TogglesTile(): JSX.Element {
-  return (
-    <div id="toggles" className="zero">
-      <h1 className="questions-title">An animal cell contains</h1>
-      <div className="questions-container">
-        <ToggleQuestion />
-      </div>
-      <p className="questions-mark">The answer is incorrect</p>
-    </div>
-  );
+    return (
+        <div id="toggles" className="zero">
+            <h1 className="questions-title">An animal cell contains</h1>
+            <div className="questions-container">
+                <ToggleQuestion />
+            </div>
+            <p className="questions-mark">The answer is incorrect</p>
+        </div>
+    );
 }
